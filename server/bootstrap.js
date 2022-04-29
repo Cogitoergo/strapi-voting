74e3a861e80a13fcd918c92c521882999ff8fb87
karolis <kstakenas@lrytas.lt>
2022-04-29 14:35:19 +0300

    Testing

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const { registerCronTasks } = require('./config/cron-tasks');
+const { fingerprint } = require('./middlewares');
 
 module.exports = ({ strapi }) => {
-	// register action check
+	fingerprint();
 	registerCronTasks({ strapi });
 };
